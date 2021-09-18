6f141b57c9ba237adaab02fc65663d3c0ebd4fcf
Yannick Severo <severoyannick@gmail.com>
2021-09-18 11:53:24 +0200

    On ajoute une fonction pour tirer un nombre aléatoire entre 1 et 6

--- js/app.js
+++ js/app.js
@@ -4,3 +4,15 @@ var diceElement = document.createElement('div');
 diceElement.classList.add('dice');
 // selection de l'id player et ajout de l'enfant diceElement
 var playerElement = document.getElementById('player').appendChild(diceElement);
+
+// Fonction generaliste pour obtenir un nombre aléatoire
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+// On ajoute une fonction pour tirer un nombre aléatoire entre 1 et 6
+function getRandomNumber6() {
+  console.log(getRandomNumber(1,6));
+}
+
+getRandomNumber6();
