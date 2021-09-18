e17c4e0f243d8a5ed2bbf8f0b0e89cfe576d3e58
Yannick Severo <severoyannick@gmail.com>
2021-09-18 16:29:50 +0200

    executer la fonction autant de fois que de dés demandés par l'utilisateur

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-// creation de la div
-var diceElement = document.createElement('div');
-// ajout de la classe dice à la div
-diceElement.classList.add('dice');
-// selection de l'id player et ajout de l'enfant diceElement
-var playerElement = document.getElementById('player').appendChild(diceElement);
-
 // Fonction generaliste pour obtenir un nombre aléatoire
 function getRandomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -12,16 +5,45 @@ function getRandomNumber(min, max) {
 
 // On ajoute une fonction pour tirer un nombre aléatoire entre 1 et 6
 function getRandomNumber6() {
-  return getRandomNumber(1,6);
+  return getRandomNumber(1, 6);
 }
 
-// On stocke le nombre aléatoire
-var randomNumber = getRandomNumber6();
+// Jouer un dé
+function rollDice() {
+  // creation de la div
+  var diceElement = document.createElement("div");
+  // ajout de la classe dice à la div
+  diceElement.classList.add("dice");
+  // selection de l'id player et ajout de l'enfant diceElement
+  document.getElementById("player").appendChild(diceElement);
 
-// On se sert du nombre obtenu pour modifier la position de l'arrière plan du dé
-var decalage = (randomNumber - 1) * -100;
-// On decale l'image aléatoirement
-diceElement.style.backgroundPositionX = decalage + "px";
+  // On stocke le nombre aléatoire
+  var randomNumber = getRandomNumber6();
 
-// Demander à l'utilisateur combien de dés il veut lancer
-var diceCount = prompt('Combien de dés ?');
+  // On se sert du nombre obtenu pour modifier la position de l'arrière plan du dé
+  var decalage = (randomNumber - 1) * -100;
+  // On decale l'image aléatoirement
+  diceElement.style.backgroundPositionX = decalage + "px";
+}
+
+// Fonction contenant les instructions à la création d'un dé
+function askDiceCount() {
+  do {
+    // Demander à l'utilisateur combien de dés il veut lancer
+    var diceCount = prompt("Combien de dés ?");
+    diceCount = parseInt(diceCount, 10);
+    console.log("Nombre entré par le user :" + diceCount);
+  } while (isNaN(diceCount));
+
+  return diceCount;
+}
+
+// executer la fonction autant de fois que de dés demandés par l'utilisateur
+function play() {
+  var diceCount = askDiceCount();
+  for (var i = 0; i < diceCount; i++) {
+    rollDice();
+  }
+}
+
+play();
